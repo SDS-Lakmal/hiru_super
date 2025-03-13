10db8c92e2e7abab56a49be2e4541517595de275
Sanjula Lakmal <sanjulalakmal548@gmail.com>
2025-03-13 12:19:10 +0530

    Added files

---
+++ lib/contact_us.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// Custom header widget
+class CustomHeader extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+
+  CustomHeader({super.key}) : preferredSize = const Size.fromHeight(45);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Column(
+        mainAxisSize: MainAxisSize.min, // Avoid extra space
+        children: [
+          const SizedBox(height: 0.1), // Move text up
+          const Text(
+            'Contact Us',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // Bold text
+              fontSize: 25, // Adjust font size
+            ),
+          ),
+        ],
+      ),
+      centerTitle: true, // Center the title in AppBar
+      backgroundColor: Colors.yellow,
+      elevation: 5.0,
+    );
+  }
+}
