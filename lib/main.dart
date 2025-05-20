af8f6b044bc7573fd59cd11ef0fe63a474a3d555
SDS-Lakmal <sanjulalakmal548@gmail.com>
2025-05-20 12:08:07 +0530

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'user_home.dart';
-import 'discount.dart';
+import 'client/user_home.dart';
+import 'admin/discount.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
