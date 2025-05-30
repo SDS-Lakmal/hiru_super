50b62adabc4337e7bd3c4a2855e26a5026e47757
SDS-Lakmal <sanjulalakmal548@gmail.com>
2025-05-30 15:40:51 +0530

    error fixed code

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// Import your screens
+// ඔබේ screen import කරන්න
 import 'client/user_home.dart';
 import 'admin/discount.dart';
 import 'client/contact_us.dart';
 import 'client/terms_and_conditions.dart';
 import 'client/about_us.dart';
-import 'client/user_login.dart';
-import 'client/welcome_page.dart';
+//import 'client/user_login.dart';
+//import 'client/welcome_page.dart';
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
-  
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,15 +26,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Grocery App',
       theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: '/',
+      initialRoute: '/home',
       routes: {
-        '/': (context) => const WelcomePage(),
-        '/login': (context) => const LoginScreen(),
-        '/admin': (context) =>  AdminUpload(),
-        '/contact': (context) =>  ContactUsPage(),
-        '/terms': (context) =>  TermsAndConditionsPage(),
-        '/about': (context) =>  AboutUsPage(),
-        '/home': (context) =>  UserHome(),
+        // '/': (context) => const WelcomePage(),
+        // '/login': (context) => const LoginScreen(),
+        '/admin': (context) => AdminUpload(),
+        '/contact': (context) => ContactUsPage(),
+        '/terms': (context) => TermsAndConditionsPage(),
+        '/about': (context) => AboutUsPage(),
+        '/home': (context) => UserHome(),
       },
     );
   }
